3ae93f5f13cabc9ec5841bc0035bb6de60852b9a
GamingProVideos <68269384+GamingProVideos@users.noreply.github.com>
2025-01-05 22:33:05 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'Gamingprovids'
+description 'GPV Supercharger Assest'
+version '1.1.0'
+lua54 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/tesla.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/electric_nozzle_typ.ytyp'
+
+
