073e114f16f1685bd4ab6a8047ae466fe5ee1d3c
GamingProVideos <68269384+GamingProVideos@users.noreply.github.com>
2025-01-08 11:25:13 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,6 @@ description 'GPV Supercharger Assest'
 version '1.1.0'
 lua54 'yes'
 
-data_file 'DLC_ITYP_REQUEST' 'stream/tesla.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/electric_nozzle_typ.ytyp'
 
 
